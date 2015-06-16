3f0b3985e85deffc29fc8c28f07c0e56ac415525
Chris Kelly <cpjkelly@gmail.com>
2015-06-16 14:26:34 -0700

    Add catch-all angular route

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var bodyParser = require('body-parser');
 
 var routes = require('./app/routes/index');
 var users = require('./app/routes/users');
+var catchAll = require('./app/routes/catchall');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/app/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -22,8 +23,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api', routes);
+app.use('/api/users', users);
+
+// forward all other routes to the angular app
+app.use('*', catchAll);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
