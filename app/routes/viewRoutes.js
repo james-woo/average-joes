5099a3840156a7808ec3960b318685345da2b6b7
Chris Kelly <cpjkelly@gmail.com>
2015-06-19 01:30:47 -0700

    Remove comment :(

--- app/routes/viewRoutes.js
+++ app/routes/viewRoutes.js
@@ -2,12 +2,6 @@ var path = require('path');
 var express = require('express');
 var router = express.Router();
 
-// exports.partials = {};
-
-// router.get("/", function(req, res, next) {
-//   res.render('layout');
-// });
-
 router.get("/users/:partial_name", function(req, res, next) {
   res.render('partials/users/' + req.params.partial_name);
 });
