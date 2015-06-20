8dadf9db48df227e9aec0fffed26d8349453a006
Chris Kelly <cpjkelly@gmail.com>
2015-06-20 15:05:42 -0700

    Require unique username for users

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,9 @@
 var mongoose = require('app/models/mongoose');
 
 var userSchema = mongoose.Schema({
-  username: String,
+  username: {type: String, index: {unique: true}},
   hashed_password: String
-})
+});
 
 var User = mongoose.model('User', userSchema);
 
