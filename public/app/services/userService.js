f24a2772c9856815e684fdc6e2cddfcbc92d312d
Chris Kelly <cpjkelly@gmail.com>
2015-06-20 15:22:19 -0700

    Add semicolon

--- public/app/services/userService.js
+++ public/app/services/userService.js
@@ -4,7 +4,7 @@ angular.module('userService', [])
 
   factory.all = function(){
     return $http.get('/api/users');
-  }
+  };
 
   factory.get = function(username){
     return $http.get('/api/users/' + username);
