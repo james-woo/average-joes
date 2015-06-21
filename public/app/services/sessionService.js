8541a7056cc9dcc16efe1df7baa372783828594c
Chris Kelly <cpjkelly@gmail.com>
2015-06-21 13:54:21 -0700

    Add currentUser factory

--- public/app/services/sessionService.js
+++ public/app/services/sessionService.js
@@ -7,4 +7,25 @@ angular.module('sessionService', [])
   };
 
   return factory;
+})
+
+.factory('CurrentUser', function($http){
+  var factory = {user: ''};
+
+  factory.update = function(){
+    $http.get('/api/session')
+    .success(function(data, status, headers, config){
+      console.log("session fetch success:");
+      console.log(data.currentUser);
+      factory.user = data.currentUser
+    })
+    .error(function(data, status, headers, config){
+      console.log("session fetch error:");
+      console.log(data);
+      factory.user = '';
+    });
+    return factory.user;
+  };
+
+  return factory;
 });
