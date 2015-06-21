bf4b55ada26e08a91144c21d1d2a10ec5d4189d6
Chris Kelly <cpjkelly@gmail.com>
2015-06-21 13:54:56 -0700

    Add session fetching route

--- app/routes/api/session.js
+++ app/routes/api/session.js
@@ -4,7 +4,16 @@ var User = require('../../models/user');
 var router = express.Router();
 
 router.post('/login', passport.authenticate('local'), function(req, res){
-  res.json({});
+  res.json({currentUser: req.user});
+});
+
+router.get("/", function(req, res){
+  if(req.user){
+    res.status(200).json({currentUser: req.user});
+  }
+  else{
+    res.status(404).json({currentUser: req.user});
+  }
 });
 
 module.exports = router;
