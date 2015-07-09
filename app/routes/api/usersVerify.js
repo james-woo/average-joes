8cf87e80334afb169d5e7ac16b082ad17ac9fe3e
James <jamesjaywoo@gmail.com>
2015-07-09 10:46:31 -0700

    Change resend email
    Changed the way to resend email, the user can now click a link, instead
    of resending a link everytime.

--- app/routes/api/usersVerify.js
+++ app/routes/api/usersVerify.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 var User = require('../../../app/models/user')
 var passport = require('passport');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'averagejoesmembers@gmail.com',
+    pass: 'mysupersecretpassword'
+  }
+});
 
 router.get('/:uid',function(req,res){
   var key = req.params.uid;
@@ -17,11 +26,34 @@ router.get('/:uid',function(req,res){
       }
       user.confirmed = "true";
       user.save();
-      res.render('layout');
+      res.redirect("/verify");
     }
   } else{
     res.redirect("/sorry");
   }
+  });
+});
+
+// send confirmation email to user with key
+router.get('/resend/:hash', function(req, res){
+  var hash = req.params.hash;
+  User.findOne({key: hash}, function(err, user){
+    var key = user.key;
+    var email = user.email;
+    host=req.get('host');
+    link="http://"+req.get('host')+"/api/verify/"+key;
+    var mailOptions = {
+      to : email,
+      subject : "Please confirm your Email account",
+      html : "Hello "+user.firstname+",<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
+    }
+    transporter.sendMail(mailOptions, function(error){
+      if(error){
+        console.log(error);
+      } else{
+        res.redirect("/confirm");
+      }
+    });
   });
 });
 
