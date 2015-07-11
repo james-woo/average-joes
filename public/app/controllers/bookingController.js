fa65c07aa83db407aaba447985a83b80ff4fced6
Chris Kelly <cpjkelly@gmail.com>
2015-07-11 11:47:30 -0700

    Fetch bookables in bookingNewController

--- public/app/controllers/bookingController.js
+++ public/app/controllers/bookingController.js
@@ -1,14 +1,23 @@
 var bookingControllers = angular.module('bookingControllers',[]);
 
-bookingControllers.controller('bookingNewController', function($http, Booking, $routeParams) {
+bookingControllers.controller('bookingNewController', function($http, Booking, Bookable, $routeParams) {
   var vm = this;
 
+  vm.facilityName = $routeParams.facilityName;
   vm.notice = "";
 
   vm.booking = {
     date: "",
     timeSlotIds: []
   };
+
+  Bookable.getByTypeName($routeParams.facilityName)
+  .success(function(data, status, headers, config){
+    vm.bookables = data.bookables;
+  })
+  .error(function(data, status, headers, config){
+    vm.notice += "Bookings could not be fetched."
+  });
 
   vm.submit = function(){
     Booking.create(vm.booking)
