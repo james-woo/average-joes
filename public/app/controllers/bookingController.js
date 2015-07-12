def6f759a1da8eff23346c5e1ebe47157f0e4030
Chris Kelly <cpjkelly@gmail.com>
2015-07-11 22:03:29 -0700

    Fetch timeSlots by date in bookingNewController

--- public/app/controllers/bookingController.js
+++ public/app/controllers/bookingController.js
@@ -1,19 +1,29 @@
 var bookingControllers = angular.module('bookingControllers',[]);
 
-bookingControllers.controller('bookingNewController', function($http, Booking, Bookable, $routeParams) {
+bookingControllers.controller('bookingNewController', function($http, Booking, Bookable, BookableTimeSlots, $routeParams) {
   var vm = this;
 
   vm.facilityName = $routeParams.facilityName;
   vm.notice = "";
+  if($routeParams.date){
+    // ensure that date format is consistent
+    vm.date = moment($routeParams.date, "YYYY-M-D").startOf("day");
+  }
+  else{
+    vm.date = moment().startOf("day");
+  }
 
   vm.booking = {
     date: "",
     timeSlotIds: []
   };
 
+
   Bookable.getByTypeName($routeParams.facilityName)
   .success(function(data, status, headers, config){
     vm.bookables = data.bookables;
+
+    BookableTimeSlots.filterByDay(vm.bookables, vm.date.day());
   })
   .error(function(data, status, headers, config){
     vm.notice += "Bookings could not be fetched."
